8f0f73511563b8720bad3d23ee8785c8262ceaa9
Amit <amitarazi12345@gmail.com>
2022-04-24 09:54:29 +0300

    second

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,14 +2,14 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "./contextApi";
 import { Home } from "./Components/Home";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_SERVER}/check`).then((res) =>
-      res.json().then((data) => console.log(data))
-    );
-  }, []);
+  // useEffect(() => {
+  //   fetch(`${process.env.REACT_APP_SERVER}/check`).then((res) =>
+  //     res.json().then((data) => console.log(data))
+  //   );
+  // }, []);
 
   return (
     <div className="App">
