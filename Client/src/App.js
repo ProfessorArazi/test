3cc09056e5c7e108f9afe34820990dc122792529
Amit <amitarazi12345@gmail.com>
2022-04-29 18:08:25 +0300

    header with toggle button

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "./contextApi";
 import { Home } from "./Components/Home";
+import { useState } from "react";
 // import { useEffect } from "react";
 
 function App() {
@@ -11,8 +12,32 @@ function App() {
   //   );
   // }, []);
 
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <div className="App">
+      <nav
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          margin: "10px 20px",
+        }}
+      >
+        <a href="#">something</a>
+        <a href="#">something</a>
+        <a href="#">something</a>
+        <button onClick={() => setOpenMenu(!openMenu)}>Menu</button>
+      </nav>
+      {openMenu && (
+        <div
+          style={{ textAlign: "right", marginRight: "20px" }}
+          onClick={() => setOpenMenu(false)}
+        >
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+        </div>
+      )}
       <Provider value={{ home: <p>Home</p> }}>
         <BrowserRouter>
           <Routes>
